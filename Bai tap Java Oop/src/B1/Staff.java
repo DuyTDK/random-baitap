70c38d546fb58dc064b156cde36ff0bac97a4645
Duy <105420834+DuyTDK@users.noreply.github.com>
2024-03-29 14:56:24 +0700

    Add files via upload

--- Bai tap Java Oop/src/B1/Staff.java
+++ Bai tap Java Oop/src/B1/Staff.java
@@ -1,5 +1,22 @@
 package B1;
 
-public class Staff {
+public class Staff extends Officer{
+	private String task;
+
+	public Staff(String name, int age, String gender, String address, String task) {
+		super(name, age, gender, address);
+		// TODO Auto-generated constructor stub
+		this.setTask(task);
+	}
+
+	public String getTask() {
+		return task;
+	}
+
+	public void setTask(String task) {
+		this.task = task;
+	}
+	
+	
 
 }
